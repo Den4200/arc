7f78299481c2f7304e12eaf484334db449a606e5
Den4200 <dpham.42@hotmail.com>
2020-05-02 01:15:43 -0400

    added default open file and saving

--- arc/editor.py
+++ arc/editor.py
@@ -1,13 +1,18 @@
+from typing import Any, Tuple
+
+from kivy.core.window import Keyboard
 from kivy.lang import Builder
 from kivy.properties import (
     ColorProperty,
     NumericProperty,
     ObjectProperty,
+    ObservableList,
     StringProperty
 )
 from kivy.uix.codeinput import CodeInput
 from kivy.uix.screenmanager import Screen
 
+from arc import KEYS
 from arc.terminal import Terminal  # NOQA: F401
 
 Builder.load_file('arc/editor.kv')
@@ -24,4 +29,39 @@ class EditorScreen(Screen):
 
 
 class Editor(CodeInput):
-    file_path = StringProperty()
+    file_path = StringProperty('arc/core.py')
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+        with open(self.file_path, 'r') as f:
+            self.text = f.read()
+
+    def keyboard_on_key_down(
+            self,
+            window: Keyboard,
+            keycode: Tuple[int, str],
+            text: str,
+            modifiers: ObservableList
+    ):
+        """
+        Captures specific key presses
+        and executes accordingly.
+        """
+        if keycode[0] in KEYS['s'] and 'ctrl' in modifiers:
+            with open(self.file_path, 'w') as f:
+                f.write(self.text)
+
+        elif keycode[0] in KEYS['del', 'backspace']:
+            self.cancel_selection()
+
+        elif keycode[0] in KEYS['='] and 'ctrl' in modifiers:
+            self.font_size += 1
+
+        elif keycode[0] in KEYS['-'] and 'ctrl' in modifiers:
+            if self.font_size > 0:
+                self.font_size -= 1
+
+        return super().keyboard_on_key_down(
+            window, keycode, text, modifiers
+        )
